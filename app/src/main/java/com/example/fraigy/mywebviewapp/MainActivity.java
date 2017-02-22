ea6306f4e8ed7ecdba3b010cc6cb0be0fe2877ea
fraigy <fraigy@naver.com>
2017-02-23 01:10:45 +0900

    Set WebView to http://beta.html5test.com/.

--- app/src/main/java/com/example/fraigy/mywebviewapp/MainActivity.java
+++ app/src/main/java/com/example/fraigy/mywebviewapp/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.fraigy.mywebviewapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Alloc WebView
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+
+        // Enable Javascript
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        // LoadUrl
+        mWebView.loadUrl("http://beta.html5test.com/");
     }
 }
